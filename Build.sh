a0b0d89db7e3fcdfd15af1f857e3b8fd1479b6a5
Aryan Karan <76065891+aryankaran@users.noreply.github.com>
2021-05-20 22:14:49 +0530

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash 
 
 
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8 arm-eabi-4.8
 
 export ARCH=arm 
 
 export SUBARCH=arm 
 
-export CROSS_COMPILE=/home/abid/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=$(pwd)/arm-eabi-4.8/bin/arm-eabi-
 
 make O=out TARGET_ARCH=arm gh6_mt6580_defconfig
 
